457b2fa4eacfdd00a1e7f3983b65bc752414cf4c
SeonghoonPark(Windows) <sunghoon8585@konkuk.ac.kr>
2025-12-15 22:53:10 +0900

    add motor driver gpio

--- src/app/vehicle_operation/vehicle_controller/Core/Inc/main.h
+++ src/app/vehicle_operation/vehicle_controller/Core/Inc/main.h
@@ -61,6 +61,8 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define USER_Btn_Pin GPIO_PIN_13
 #define USER_Btn_GPIO_Port GPIOC
+#define RIGHT_IN2_Pin GPIO_PIN_3
+#define RIGHT_IN2_GPIO_Port GPIOF
 #define MCO_Pin GPIO_PIN_0
 #define MCO_GPIO_Port GPIOH
 #define RMII_MDC_Pin GPIO_PIN_1
@@ -77,6 +79,14 @@ void Error_Handler(void);
 #define RMII_RXD1_GPIO_Port GPIOC
 #define LD1_Pin GPIO_PIN_0
 #define LD1_GPIO_Port GPIOB
+#define LEFT_IN1_Pin GPIO_PIN_12
+#define LEFT_IN1_GPIO_Port GPIOF
+#define LEFT_IN2_Pin GPIO_PIN_13
+#define LEFT_IN2_GPIO_Port GPIOF
+#define STEER_IN2_Pin GPIO_PIN_14
+#define STEER_IN2_GPIO_Port GPIOF
+#define RIGHT_IN1_Pin GPIO_PIN_15
+#define RIGHT_IN1_GPIO_Port GPIOF
 #define RMII_TXD1_Pin GPIO_PIN_13
 #define RMII_TXD1_GPIO_Port GPIOB
 #define LD3_Pin GPIO_PIN_14
@@ -85,6 +95,8 @@ void Error_Handler(void);
 #define STLK_RX_GPIO_Port GPIOD
 #define STLK_TX_Pin GPIO_PIN_9
 #define STLK_TX_GPIO_Port GPIOD
+#define STEER_IN1_Pin GPIO_PIN_15
+#define STEER_IN1_GPIO_Port GPIOD
 #define USB_PowerSwitchOn_Pin GPIO_PIN_6
 #define USB_PowerSwitchOn_GPIO_Port GPIOG
 #define USB_OverCurrent_Pin GPIO_PIN_7
